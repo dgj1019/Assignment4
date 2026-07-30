0a5c3412e4bf378c8f08cd0e5c0df5284e8d7195
Daniel <daniel13_2012@yahoo.com>
2026-07-29 19:55:02 -0500

    Implement AccountNumberComparator

--- SPCS3354/AccountNumberComparator.java
+++ SPCS3354/AccountNumberComparator.java
@@ -1,1 +1,18 @@
+package SPCS3354;
 
+import java.util.Comparator;
+
+public class AccountNumberComparator implements Comparator<BankAccount> {
+  @Override
+  public int compare(BankAccount a1, BankAccount a2){
+        if (a1.getNumber() < a2.getNumber()) {
+            return -1;
+        }
+        else if (a1.getNumber() == a2.getNumber()) {
+            return 0;
+        }
+        else {
+            return 1;
+        }    
+  }
+}
