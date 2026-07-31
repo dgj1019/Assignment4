a146b436cbe5031c29c53a5c30b49034afc3a6ba
Daniel <daniel13_2012@yahoo.com>
2026-07-30 23:30:20 -0500

    Update comments

--- SPCS3354/AccountNumberComparator.java
+++ SPCS3354/AccountNumberComparator.java
@@ -3,19 +3,19 @@ package SPCS3354;
 import java.util.Comparator;
 
 /**
- * Compares two bank accounts by account numbers.
- */
+* Compares two bank accounts by account numbers.
+*/
 public class AccountNumberComparator implements Comparator<BankAccount> {
 
   /**
- * Compares two bank accounts based on their account numbers.
- *
- * @param a1 first bank account
- * @param a2 second bank account
- * @return a negative integer, zero, or a positive integer if the first
- * account number is less than, equal to, or greater respectively 
- * than the second
- */
+  * Compares two bank accounts based on their account numbers.
+  *
+  * @param a1 first bank account
+  * @param a2 second bank account
+  * @return a negative integer, zero, or a positive integer if the first
+  * account number is less than, equal to, or greater respectively 
+  * than the second
+  */
   @Override
   public int compare(BankAccount a1, BankAccount a2){
         if (a1.getNumber() < a2.getNumber()) {
