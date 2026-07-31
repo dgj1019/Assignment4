fd33efa1bb76200299cf6e60a34e901b0532e83e
GitMeOutta-Here <jackgrossenbacher@gmail.com>
2026-07-30 22:50:31 -0500

    Improved clarity with try-catch block, and added JavaDoc comment to SPAssignment4

--- SPCS3354/SPAssignment4.java
+++ SPCS3354/SPAssignment4.java
@@ -7,6 +7,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * SPAssignment4 comaints the main method for the SPCS3354 package. The SPCS3354 package
+ * acts as a way to parse and sort bank information from properly formatted input files.
+ * 
+ * <p> 
+ * Main method first checks the length of args, if length != 2 -> too many/litte args. If
+ * the right number of args are present, create an array list of the BankAccount type, and
+ * two File objects to hold the input/output files. 
+ * 
+ * Then a try-catch block is used with a Scanner and PrintWriter object to read/write information
+ * from the files, and catch a FileNotFoundException which means the user input the wrong fileName,
+ * or the file does not exit.
+ * 
+ * Then, while there is a line to read, I create a String object to store the token, then another
+ * try-catch block.
+ * 
+ * In the second try-catch block each line is parsed into a String array using the ',' as a seperator. I can then
+ * read each field from the array into variables of their specific type, using the wrapper class methods
+ * Integer.parseInt & Double.parseDouble to type-cast those string values so that they can be stored appropriately.
+ * 
+ * If-else statements then check which account type we want to create, and create the respective interest & checkLim
+ * variables. Then each account is added to the BankAccount ArrayList. If the parsing of the line goes wrong in some 
+ * form: NumberFormatException, ArrayIndexOutOfBounds, etc.. the catch block will catch the exception and print the 
+ * problematic line to the terminal.
+ * 
+ * Finally, each succesfully created SavingsAccount & CheckingAccount object is sorted using the custom AccountNumberComparator
+ * and written to the output file using the PrintWriter object. Finally the program will print Bye!
+ * 
+ * @param args Contains the names of the input/output files stored in a String array
+ * 
+ * @author Jack Grossenbacher
+ * @author Daniel Gallegos
+ * @version 1.0.0
+ */
+
 
 public class SPAssignment4 {
     public static void main(String[] args) {
@@ -21,8 +56,7 @@ public class SPAssignment4 {
         File inputFile = new File(args[0]);
         File outputFile = new File(args[1]);
 
-        try (Scanner scanner = new Scanner(inputFile)) {
-            PrintWriter writer = new PrintWriter(outputFile);
+        try (Scanner scanner = new Scanner(inputFile); PrintWriter writer = new PrintWriter(outputFile)) {
             AccountNumberComparator accCompare = new AccountNumberComparator();
 
                 while(scanner.hasNext()) {
