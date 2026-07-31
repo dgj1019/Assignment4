946f41d65777c9fd293783a1b97bc5266e989eb2
Daniel <daniel13_2012@yahoo.com>
2026-07-30 23:32:43 -0500

    added Javadoc comments

--- SPCS3354/SavingsAccount.java
+++ SPCS3354/SavingsAccount.java
@@ -2,18 +2,35 @@ package SPCS3354;
 
 import java.io.PrintWriter;
 
+/**
+* Represents a savings account.
+*/
 public class SavingsAccount extends BankAccount {
     private double interest;
 
+    /**
+    * Creates an empty savings account.
+    */
     public SavingsAccount() {
         super();
     }
 
+    /**
+    * Creates a savings account with the specified information.
+    *
+    * @param owner the account owner's name
+    * @param number the account number
+    * @param balance the account balance
+    * @param interest the interest rate
+    */
     public SavingsAccount(String owner, int number, double balance, double interest) {
         super(owner, number, balance);
         this.interest = interest;
     }
 
+    /**
+    * Prints the savings account information to the console.
+    */
     @Override
     public void printBankAccount() {
         System.out.println(owner + "," + number + "," + balance + ",savings," + interest);
